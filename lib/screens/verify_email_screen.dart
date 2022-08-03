7927719bc26e8c8e89356eed5561de5fc5c18c96
theVatsal3802 <vatsaladhiya248@gmail.com>
2022-08-03 15:59:31 +0530

    Fixed Timer Bugs

--- lib/screens/verify_email_screen.dart
+++ lib/screens/verify_email_screen.dart
@@ -18,7 +18,7 @@ class VerifyEmailScreen extends StatefulWidget {
 class _VerifyEmailScreenState extends State<VerifyEmailScreen> {
   bool isEmailVerified = false;
   bool canResendEmail = false;
-  Timer? timer;
+  Timer timer = Timer(const Duration(seconds: 0), () {});
   User? user = FirebaseAuth.instance.currentUser;
 
   @override
@@ -69,7 +69,7 @@ class _VerifyEmailScreenState extends State<VerifyEmailScreen> {
   @override
   void dispose() {
     super.dispose();
-    timer!.cancel();
+    timer.cancel();
   }
 
   Future<void> checkEmailVerified() async {
@@ -79,7 +79,7 @@ class _VerifyEmailScreenState extends State<VerifyEmailScreen> {
         isEmailVerified = FirebaseAuth.instance.currentUser!.emailVerified;
       });
       if (isEmailVerified) {
-        timer!.cancel();
+        timer.cancel();
       }
     } catch (e) {
       ScaffoldMessenger.of(context).showSnackBar(
