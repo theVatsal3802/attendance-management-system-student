89774cbe5de8bed7efcc946442e66eadfad24b55
theVatsal3802 <vatsaladhiya248@gmail.com>
2022-08-03 20:17:41 +0530

    Improved UX

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -29,6 +29,7 @@ class DashBoardScreen extends StatefulWidget {
 }
 
 class _DashBoardScreenState extends State<DashBoardScreen> {
+  bool scanned = false;
   late StreamSubscription subscription;
   bool isConnected = false;
   bool isAlertSet = false;
@@ -168,6 +169,7 @@ class _DashBoardScreenState extends State<DashBoardScreen> {
       );
       setState(() {
         result = cameraScanResult;
+        scanned = true;
       });
       String month = DateHelper().setMonth(DateTime.now().month.toString());
       String teacherName = cameraScanResult.substring(17);
@@ -246,110 +248,142 @@ class _DashBoardScreenState extends State<DashBoardScreen> {
           );
         }
         return Scaffold(
-          appBar: AppBar(
-            title: const Text(
-              "Welcome",
-              textScaleFactor: 1,
-            ),
-            actions: [
-              IconButton(
-                tooltip: "Logout",
-                onPressed: signOut,
-                icon: const Icon(
-                  Icons.logout,
-                ),
-              ),
-            ],
-          ),
-          body: SingleChildScrollView(
-            child: Stack(
-              alignment: Alignment.center,
-              children: [
-                Center(
-                  child: Opacity(
-                    opacity: 0.6,
-                    child: Image.asset(
-                      "assets/images/logo.png",
-                      height: 200,
-                      width: 200,
-                      fit: BoxFit.cover,
-                    ),
+          appBar: scanned
+              ? AppBar(
+                  title: const Text(
+                    "Processing...",
+                    textScaleFactor: 1,
                   ),
+                )
+              : AppBar(
+                  title: const Text(
+                    "Welcome",
+                    textScaleFactor: 1,
+                  ),
+                  actions: [
+                    IconButton(
+                      tooltip: "Logout",
+                      onPressed: signOut,
+                      icon: const Icon(
+                        Icons.logout,
+                      ),
+                    ),
+                  ],
                 ),
-                SizedBox(
-                  height: height,
-                  width: width,
-                  child: Padding(
-                    padding: const EdgeInsets.all(20),
+          body: scanned
+              ? Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: SizedBox(
+                    height: height,
+                    width: width,
                     child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
-                        HeadingText(
-                          text: "Hello, ${snapshot.data!["name"]}",
-                          textSize: 32,
-                        ),
-                        VerticalSizedBox(height: height * 0.02),
-                        CustomTabButton(
-                          onTap: () {
-                            _scanQR(snapshot.data!["name"]);
-                          },
-                          icon: Icons.qr_code_scanner,
-                          child: Text(
-                            "Mark Attendance",
-                            textScaleFactor: 1,
-                            style: TextStyle(
-                              fontSize: Theme.of(context)
-                                  .textTheme
-                                  .headline6!
-                                  .fontSize,
-                              color: Theme.of(context).colorScheme.secondary,
-                            ),
-                          ),
-                        ),
-                        VerticalSizedBox(height: height * 0.05),
-                        CustomTabButton(
-                          onTap: () {
-                            Navigator.of(context)
-                                .pushNamed(AttendanceRecordScreen.routeName);
-                          },
-                          icon: Icons.stacked_bar_chart,
-                          child: Text(
-                            "See Attendance Record",
-                            textScaleFactor: 1,
-                            style: TextStyle(
-                              fontSize: Theme.of(context)
-                                  .textTheme
-                                  .headline6!
-                                  .fontSize,
-                              color: Theme.of(context).colorScheme.secondary,
-                            ),
-                          ),
-                        ),
-                        VerticalSizedBox(height: height * 0.05),
-                        CustomTabButton(
-                          onTap: () {
-                            Navigator.of(context)
-                                .pushNamed(EditProfileScreen.routeName);
-                          },
-                          icon: Icons.edit,
-                          child: Text(
-                            "Edit Profile",
-                            textScaleFactor: 1,
-                            style: TextStyle(
-                              fontSize: Theme.of(context)
-                                  .textTheme
-                                  .headline6!
-                                  .fontSize,
-                              color: Theme.of(context).colorScheme.secondary,
-                            ),
-                          ),
+                        const CircularProgressIndicator.adaptive(),
+                        const VerticalSizedBox(height: 20,),
+                        Text(
+                          "Sending Your attendance for evaluation",
+                          textScaleFactor: 1,
+                          style: Theme.of(context).textTheme.headline6,
+                          textAlign: TextAlign.center,
                         ),
                       ],
                     ),
                   ),
+                )
+              : SingleChildScrollView(
+                  child: Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      Center(
+                        child: Opacity(
+                          opacity: 0.6,
+                          child: Image.asset(
+                            "assets/images/logo.png",
+                            height: 200,
+                            width: 200,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: height,
+                        width: width,
+                        child: Padding(
+                          padding: const EdgeInsets.all(20),
+                          child: Column(
+                            children: [
+                              HeadingText(
+                                text: "Hello, ${snapshot.data!["name"]}",
+                                textSize: 32,
+                              ),
+                              VerticalSizedBox(height: height * 0.02),
+                              CustomTabButton(
+                                onTap: () {
+                                  _scanQR(snapshot.data!["name"]);
+                                },
+                                icon: Icons.qr_code_scanner,
+                                child: Text(
+                                  "Mark Attendance",
+                                  textScaleFactor: 1,
+                                  style: TextStyle(
+                                    fontSize: Theme.of(context)
+                                        .textTheme
+                                        .headline6!
+                                        .fontSize,
+                                    color:
+                                        Theme.of(context).colorScheme.secondary,
+                                  ),
+                                ),
+                              ),
+                              VerticalSizedBox(height: height * 0.05),
+                              CustomTabButton(
+                                onTap: () {
+                                  Navigator.of(context).pushNamed(
+                                      AttendanceRecordScreen.routeName);
+                                },
+                                icon: Icons.stacked_bar_chart,
+                                child: Text(
+                                  "See Attendance Record",
+                                  textScaleFactor: 1,
+                                  style: TextStyle(
+                                    fontSize: Theme.of(context)
+                                        .textTheme
+                                        .headline6!
+                                        .fontSize,
+                                    color:
+                                        Theme.of(context).colorScheme.secondary,
+                                  ),
+                                ),
+                              ),
+                              VerticalSizedBox(height: height * 0.05),
+                              CustomTabButton(
+                                onTap: () {
+                                  Navigator.of(context)
+                                      .pushNamed(EditProfileScreen.routeName);
+                                },
+                                icon: Icons.edit,
+                                child: Text(
+                                  "Edit Profile",
+                                  textScaleFactor: 1,
+                                  style: TextStyle(
+                                    fontSize: Theme.of(context)
+                                        .textTheme
+                                        .headline6!
+                                        .fontSize,
+                                    color:
+                                        Theme.of(context).colorScheme.secondary,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              ],
-            ),
-          ),
         );
       },
     );
